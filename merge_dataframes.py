02af7f7e38fb47652ecc131a1d9a6ca4c118b4bf
Scott Huberty <seh33@uw.edu>
2024-07-12 16:26:38 -0700

    FIX: make it work again

--- merge_dataframes.py
+++ merge_dataframes.py
@@ -6,10 +6,13 @@ sns.set_theme(style="darkgrid")
 
 # read csv files
 df_newborn = pd.read_csv("./csv/participants_newborn.csv", index_col="study_id")
-df_sixmonth = pd.read_csv("participants_six_month.csv", index_col="study_id")
-derivatives_newborn = pd.read_csv("derivative_newborn.csv", index_col="study_id")
-derivatives_sixmonth = pd.read_csv("derivative_six_month.csv", index_col="study_id")
-df_redcap = pd.read_csv("redcap.csv", usecols=["study_id"], dtype="string")
+df_sixmonth = pd.read_csv("./csv/participants_six_month.csv", index_col="study_id")
+
+derivatives_newborn = pd.read_csv("./csv/derivatives_newborn.csv", index_col="study_id")
+derivatives_sixmonth = pd.read_csv("./csv/derivatives_six_month.csv", index_col="study_id")
+
+
+df_redcap = pd.read_csv("./csv/redcap.csv", usecols=["study_id"], dtype="string")
 # Drop Duplicate Study ID's in index
 df_redcap = df_redcap.drop_duplicates("study_id")
 # Drop study ID's that are not 1000-1999
@@ -33,25 +36,28 @@ df_acquired = pd.concat(
 # df_acquired.columns = df_acquired.columns.set_levels(["Visit"], level=0)
 # df_acquired.columns = df_acquired.columns.set_levels(["Visit", "Scan"], level=1)
 df_nibabies = pd.concat(
-    [nibabies_newborn, nibabies_sixmonth],
+    [derivatives_newborn, derivatives_sixmonth],
     axis=1,
     keys=["Newborn", "Six Months"],
     names=["Visit", "Scan"],
 )
-df_nibabies = df_nibabies.fillna(False)
+
+with pd.option_context("future.no_silent_downcasting", True):
+    df_nibabies = df_nibabies.fillna(False).astype(bool)
 
 df_babies = pd.concat(
     [df_acquired, df_nibabies],
     axis=1,
-    keys=["Acquired", "NiBabies"],
+    keys=["Acquired", "Processed"],
     names=["Stage", "Visit", "Scan"],
 )
 
-df_babies = df_babies.fillna(False)
+with pd.option_context("future.no_silent_downcasting", True):
+    df_babies = df_babies.fillna(False).astype(bool)
 
-report = df_nibabies.T.groupby(level=[0, 1]).sum().sum(axis=1)
+# report = df_nibabies.T.groupby(level=[0, 1]).sum().sum(axis=1)
 
-report_babies = df_babies.T.groupby(level=[0, 1, 2]).sum().sum(axis=1)
+# report_babies = df_babies.T.groupby(level=[0, 1, 2]).sum().sum(axis=1)
 
 # Save DataFrames to csv
 df_acquired.to_csv(
@@ -71,18 +77,18 @@ sns.set_theme(style="darkgrid")
 
 fig, ax = plt.subplots(figsize=(10, 5), constrained_layout=True)
 
-data = df_acquired.fillna(False).groupby(level=[0]).sum().sum(axis=0)
-report = df_acquired.fillna(False).groupby(level=[0]).sum().sum(axis=0).plot(kind="bar", ax=ax)
-
 # Grouped bar chart of Scan counts, X axis Scan type, Hue by visit,
 to_drop = [("Acquired", visit, scan) for visit in ["Newborn", "Six Months"] for scan in ["T1w", "T2w"]]
-data = df_babies.T.drop("NiBabies").drop(to_drop)
+data = df_babies.T.drop("Processed").drop(to_drop)
 data = data.groupby(level=[0, 1, 2]).sum().sum(axis=1).to_frame(name="Value").stack().reset_index().rename(columns={0: "Value"})
 
 with sns.color_palette("pastel"):
     sns.barplot(x="Scan", y="Value", hue="Visit", data=data, ax=ax)
 
-data = df_babies.T.drop("Acquired").groupby(by=["Visit", "Scan"]).sum().sum(axis=1)
+data.to_csv("./reports/scan_counts.csv")
+
+to_drop = [("Processed", visit, col) for visit in ["Newborn", "Six Months"] for col in ["Surface-Recon-Method", "Date-Processed", "Precomputed", "Recon-all"]]
+data = df_babies.T.drop("Acquired").drop(to_drop).groupby(by=["Visit", "Scan"]).sum().sum(axis=1)
 data = data.to_frame().stack().reset_index().rename(columns={0: "Value"})
 with sns.color_palette("muted"):
     sns.barplot(
@@ -94,4 +100,8 @@ with sns.color_palette("muted"):
         linewidth=2.5,
         edgecolor=".5",
         )
+data.to_csv("./reports/processed_counts.csv")
+fig.savefig("./reports/scan_counts.png")
 
+#cond_new = ((df_babies[("Acquired", "Newborn", "Anatomical")] == True
+# cond_six = ((df_babies[("Acquired", "Six Months", "Anatomical")] == True) |
